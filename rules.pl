d455a8dc29460acc6efe58af0c40fb0be260ddb1
Chocolate <namtan8888@gmail.com>
2016-10-21 10:09:29 +0700

    Added complex filter

--- rules.pl
+++ rules.pl
@@ -17,6 +17,15 @@ place_in_act(ACTIVITY, PLACE):-
 	facts:place(PLACE),
 	facts:has_act(PLACE, ACTIVITY).
 
+place_with_custom_filters(PLACE, DISTRICT, TYPE, ACTIVITY, AGE, EN_DIS, EN_TYPE, EN_ACTIVITY, EN_ACT_AGE):-
+	((EN_DIS, place_in_district(DISTRICT, PLACE)); not(EN_DIS)),
+	((EN_TYPE, place_in_type(TYPE, PLACE)); not(EN_TYPE)),
+	((
+		EN_ACTIVITY,
+		place_in_act(ACTIVITY, PLACE),
+		((EN_ACT_AGE, act_for_age(ACTIVITY, AGE)); not(EN_ACT_AGE))
+	); not(EN_ACTIVITY)).
+
 act_for_age(ACTIVITY, AGE):-
 	facts:act_age_range_end(ACTIVITY, LIMIT_UPPER_AGE),
 	facts:act_age_range_start(ACTIVITY, LIMIT_LOWER_AGE),
@@ -36,27 +45,4 @@ is_open(PLACE,T1,T2):-
 is_enough_budget(AMOUNT,PLACE):-
 	facts:budget(PLACE,AMOUNT_B),
 	gte(AMOUNT,AMOUNT_B).
-	
-list_place_in_district(X):-
-	facts:place_in_district(X,Y),
-	write(Y),
-	nl,
-    fail
-    ;
-    true.
 
-list_place_in_type(X):-
-	facts:place_in_type(X,Y),
-	write(Y),
-	nl,
-    fail
-    ;
-    true.
-
-query_dis_type_act(DIS, TYPE, ACT, PLACE, ENABLE_QUERY_1, ENABLE_QUERY_2, ENABLE_QUERY_3):-
-	(
-		(ENABLE_QUERY_1, place_in_district(DIS,PLACE));
-		(ENABLE_QUERY_2, place_in_type(TYPE,PLACE));
-		(ENABLE_QUERY_3, place_in_act(ACT,PLACE))
-	),
-	write(PLACE), nl, fail;true.
