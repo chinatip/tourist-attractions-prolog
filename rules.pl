bf3bda34bc5a350af812ba11a393a38e532bd743
Chocolate <namtan8888@gmail.com>
2016-10-24 22:25:00 +0700

    Added entrance fee to complex filter

--- rules.pl
+++ rules.pl
@@ -17,16 +17,17 @@ place_in_act(ACTIVITY, PLACE):-
 	facts:place(PLACE),
 	facts:has_act(PLACE, ACTIVITY).
 
-place_with_custom_filters(PLACE, DISTRICT, TYPE, ACTIVITY, AGE, TIME, BUDGET, EN_DIS, EN_TYPE, EN_ACTIVITY, EN_ACT_AGE, EN_TIME, EN_BUDGET):-
-	((EN_TIME, time_for_place(PLACE, TIME)); not(EN_TIME)),
-	((EN_BUDGET, is_enough_budget(BUDGET,PLACE)); not(EN_BUDGET)),
+place_with_custom_filters(PLACE, DISTRICT, TYPE, ACTIVITY, AGE, TIME, BUDGET, ENTRANCE_FEE, EN_DIS, EN_TYPE, EN_ACTIVITY, EN_ACT_AGE, EN_TIME, EN_BUDGET, EN_ENT_FEE):-
 	((EN_DIS, place_in_district(DISTRICT, PLACE)); not(EN_DIS)),
 	((EN_TYPE, place_in_type(TYPE, PLACE)); not(EN_TYPE)),
 	((
 		EN_ACTIVITY,
 		place_in_act(ACTIVITY, PLACE),
 		((EN_ACT_AGE, act_for_age(ACTIVITY, AGE)); not(EN_ACT_AGE))
-	); not(EN_ACTIVITY)).
+	); not(EN_ACTIVITY)),
+	((EN_TIME, time_for_place(PLACE, TIME)); not(EN_TIME)),
+	((EN_BUDGET, is_enough_budget(BUDGET,PLACE)); not(EN_BUDGET)),
+	((EN_ENT_FEE, is_enough_entrance_fee(ENTRANCE_FEE,PLACE)); not(EN_ENT_FEE)).
 
 act_for_age(ACTIVITY, AGE):-
 	facts:act_age_range_end(ACTIVITY, LIMIT_UPPER_AGE),
@@ -44,7 +45,10 @@ is_open(PLACE,T1,T2):-
 	time_for_place(PLACE, T1),
 	time_for_place(PLACE, T2).
 
-is_enough_budget(AMOUNT,PLACE):-
-	facts:budget(PLACE,AMOUNT_B),
-	gte(AMOUNT,AMOUNT_B).
+is_enough_budget(BUDGET,PLACE):-
+	facts:budget(PLACE,AMOUNT),
+	gte(BUDGET,AMOUNT).
 
+is_enough_entrance_fee(ENT_FEE,PLACE):-
+	facts:entrance_fee(PLACE,AMOUNT),
+	gte(ENT_FEE,AMOUNT).
